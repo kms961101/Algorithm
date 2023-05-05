ba5e52931306fb8637590f5db397b34e3c681a87
kms961101 <kms961101@nate.com>
2023-05-05 23:11:47 +0900

    230505 숫자 문자열과 영단어

---
+++ Algorithm/Programmers/Level 1/숫자 문자열과 영단어.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        String[] wordToNumber = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        
+        for(int i = 0; i < wordToNumber.length; i++){
+            s = s.replaceAll(wordToNumber[i], Integer.toString(i));
+        }
+        
+        return Integer.parseInt(s);
+    }
+}
