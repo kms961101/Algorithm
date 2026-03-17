23f209c09b813cbc5ad81a0f98e866203ef8ab83
kms961101 <63328574+kms961101@users.noreply.github.com>
2026-03-17 20:50:45 +0900

    Title: 1, 2, 5 더하기, Time: 136ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260317/1, 2, 5 더하기/1-2-5-plus.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int mod = 10007;
+        int[] dp = new int[n + 1];
+        int[] arr = {1, 2, 5};
+        dp[0] = 1;
+        for(int i = 1; i <= n; i++) {
+        	for(int j = 0; j < 3; j++) {
+        		if(i - arr[j] < 0) continue;
+        		dp[i] += dp[i - arr[j]];
+        	}
+        }
+        
+        System.out.println(dp[n]);
+    }
+}
