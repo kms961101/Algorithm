d6323dfa101dc6eea355c10b61e5582141fcf124
kms961101 <63328574+kms961101@users.noreply.github.com>
2026-03-17 20:52:29 +0900

    Title: 1, 2, 5 더하기, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

--- 260317/1, 2, 5 더하기/1-2-5-plus.java
+++ 260317/1, 2, 5 더하기/1-2-5-plus.java
@@ -10,7 +10,7 @@ public class Main {
         for(int i = 1; i <= n; i++) {
         	for(int j = 0; j < 3; j++) {
         		if(i - arr[j] < 0) continue;
-        		dp[i] += dp[i - arr[j]] % 10007;
+        		dp[i] = (dp[i] + dp[i - arr[j]]) % 10007;
         	}
         }
         
