701414211628316d0cea9421c3398b3c2d5b3c4c
kms961101 <63328574+kms961101@users.noreply.github.com>
2026-03-17 21:04:05 +0900

    Title: 최대 동전 거슬러주기, Time: 114ms, Memory: 9MB, Status: Wrong Answer - Codetree

--- 260317/최대 동전 거슬러주기/max-coin-change.java
+++ 260317/최대 동전 거슬러주기/max-coin-change.java
@@ -8,7 +8,7 @@ public class Main {
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
 		int[] arr = new int[n];
-		int[] dp = new int[m + 1];
+		int[] dp = new int[10001];
 		st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < n; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
