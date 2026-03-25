ba704674a274615ed759aa6db0b4ac9eb5eb987f
kms961101 <63328574+kms961101@users.noreply.github.com>
2026-03-25 22:14:54 +0900

    Title: 막대기 나누기, Time: 254ms, Memory: 15MB, Status: Passed - Codetree

--- 260325/막대기 나누기/rod-cutting.java
+++ 260325/막대기 나누기/rod-cutting.java
@@ -11,8 +11,8 @@ public class Main {
         for(int i = 1; i <= N; i++) arr[i] = Integer.parseInt(st.nextToken());
         
         for(int i = 1; i <= N; i++) {
-        	for(int j = i; j <= N; j += i) {
-        		dp[j] = Math.max(dp[j], dp[j - i] + arr[i]);
+        	for(int j = 1; j <= i; j++) {
+        		dp[i] = Math.max(dp[i], dp[i - j] + arr[j]);
         	}
         }
         int ans = Arrays.stream(dp).max().getAsInt();
