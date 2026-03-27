f749a0bdc915f438b60fb3511ba0c0e91b803059
kms961101 <63328574+kms961101@users.noreply.github.com>
2026-03-27 19:32:49 +0900

    Title: 최소 차 분할, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260327/최소 차 분할/minimum-diff-partition.java
+++ 260327/최소 차 분할/minimum-diff-partition.java
@@ -3,36 +3,35 @@ import java.util.*;
 
 public class Main {
     static int n, total, min = 987654321;
+	static int MAX_N = 100, MAX_M = 100000;
 	static int[] arr;
-	static boolean[] selected;
+	static boolean[][] dp;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         arr = new int[n + 1];
-        selected = new boolean[n + 1];
-        
+        dp = new boolean[MAX_N + 1][MAX_M + 1];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 1; i <= n; i++) arr[i] = Integer.parseInt(st.nextToken());
         total = Arrays.stream(arr).sum();
-        powerSet(1);
+        
+        for(int i = 0; i < MAX_N; i++) Arrays.fill(dp[i], false);
+        dp[0][0] = true;
+        
+        for(int i = 1; i <= n; i++) {
+        	for(int m = 1; m <= total; m++) {
+        		// 1. i번째 선택하는 경우
+        		if(m - arr[i] >= 0 && dp[i - 1][m - arr[i]]) dp[i][m] = true;
+        		// 2. i번 선택 안해도 무게 m이 가능할 경우
+        		if(dp[i - 1][m]) dp[i][m] = true;
+        	}
+        }
+        
+        for(int i = 1; i <= total; i++) {
+        	min = Math.min(min, Math.abs(i - (total - i)));
+        }
+        
         System.out.println(min);
-    }
     
-    static void powerSet(int idx) {
-    	if(idx == n) {
-    		int left = 0;
-    		for(int i = 1; i <= n; i++) {
-    			if(selected[i]) left += arr[i];
-    		}
-    		int right = total - left;
-    		min = Math.min(min, Math.abs(right - left));
-    		return;
-    	}
-    	
-    	selected[idx] = false;
-    	powerSet(idx + 1);
-    	
-    	selected[idx] = true;
-    	powerSet(idx + 1);
     }
 }
