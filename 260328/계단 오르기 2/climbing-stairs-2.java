03eec3eb73831c31afbb278e4b69557327e3f698
kms961101 <63328574+kms961101@users.noreply.github.com>
2026-03-28 23:14:29 +0900

    Title: 계단 오르기 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260328/계단 오르기 2/climbing-stairs-2.java
+++ 260328/계단 오르기 2/climbing-stairs-2.java
@@ -9,21 +9,21 @@ public class Main {
         int[] arr = new int[N + 1];
         int[][] dp = new int[4][N + 1];
         for(int i = 1; i <= N; i++) arr[i] = Integer.parseInt(st.nextToken());
-        for(int i = 0; i <= 3; i++) Arrays.fill(dp[i], Integer.MIN_VALUE);
-        for(int i = 0; i <= N; i += 2) dp[0][i] = arr[i];
+        dp[1][1] = arr[1];
+        dp[0][2] = arr[2];
+        dp[2][2] = arr[1] + arr[2];
         
         for(int i = 0; i <= 3; i++) {
-        	for(int j = 1; j <= N; j++) {
+        	for(int j = 3; j <= N; j++) {
         		// 1칸 넘어서 높이 j가 되는 경우
-        		if(i > 0) dp[i][j] = Math.max(dp[i - 1][j - 1] + arr[j],dp[i][j]);
+        		if(i > 0 && dp[i - 1][j - 1] != 0) dp[i][j] = Math.max(dp[i - 1][j - 1] + arr[j],dp[i][j]);
         		// 2칸 넘어서 높이 j가 되는 경우
-        		if(j - 2 >= 0) dp[i][j] = Math.max(dp[i][j - 2] + arr[j], dp[i][j]);
+        		if(dp[i][j - 2] != 0) dp[i][j] = Math.max(dp[i][j - 2] + arr[j], dp[i][j]);
         	}
         }
         int ans = 0;
         for(int i = 0; i <= 3; i++) {
-        	int max = Arrays.stream(dp[i]).max().getAsInt();
-        	ans = Math.max(ans, max);
+        	ans = Math.max(ans, dp[i][N]);
         }
         System.out.println(ans);
 	}
