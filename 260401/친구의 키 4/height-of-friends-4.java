f5192d0b1275904fd67f1fe661b2f6971bfc2c4b
kms961101 <63328574+kms961101@users.noreply.github.com>
2026-04-01 21:32:28 +0900

    Title: 친구의 키 4, Time: 0ms, Memory: 0MB, Status: Runtime Error - Codetree

--- 260401/친구의 키 4/height-of-friends-4.java
+++ 260401/친구의 키 4/height-of-friends-4.java
@@ -26,7 +26,7 @@ public class Main {
         indegree = new int[n + 1];
         visited = new boolean[n + 1];
         edge = new ArrayList[n + 1];
-        for(int i = 1; i <= n; i++) {
+        for(int i = 1; i <= m; i++) {
         	st = new StringTokenizer(br.readLine());
         	int x = Integer.parseInt(st.nextToken());
         	int y = Integer.parseInt(st.nextToken());
