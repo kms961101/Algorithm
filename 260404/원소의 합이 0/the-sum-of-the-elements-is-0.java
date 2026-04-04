e434eaae9cfc6fde25dba3f43bb0785d7adac614
kms961101 <63328574+kms961101@users.noreply.github.com>
2026-04-04 16:58:50 +0900

    Title: 원소의 합이 0, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260404/원소의 합이 0/the-sum-of-the-elements-is-0.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        long[] a = Arrays.stream(br.readLine().split(" ")).mapToLong(Long::parseLong).toArray();
+        long[] b = Arrays.stream(br.readLine().split(" ")).mapToLong(Long::parseLong).toArray();
+        long[] c = Arrays.stream(br.readLine().split(" ")).mapToLong(Long::parseLong).toArray();
+        long[] d = Arrays.stream(br.readLine().split(" ")).mapToLong(Long::parseLong).toArray();
+        
+        HashMap<Long, Integer> ab = new HashMap<>();
+        HashMap<Long, Integer> cd = new HashMap<>();
+        for(int i = 0; i < N; i++) {
+        	for(int j = 0; j < N; j++) {
+        		int abCnt = ab.getOrDefault(a[i] + b[j], 0);
+        		ab.put(a[i] + b[j], abCnt + 1);
+        		int cdCnt = ab.getOrDefault(c[i] + d[j], 0);
+        		cd.put(c[i] + d[j], cdCnt + 1);
+        	}
+        }
+        
+        int ans = 0;
+        for(long key : ab.keySet()) {
+        	if(cd.containsKey(key * -1)) {
+        		ans += ab.get(key) * cd.get(key * -1);
+        	}
+        }
+        
+        System.out.println(ans);
+    }
+}
