c0c1f6f17223cc15683d40e0750eea48d72c7020
kms961101 <63328574+kms961101@users.noreply.github.com>
2026-04-05 15:34:31 +0900

    Title: 최대로 연속한 숫자, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260405/최대로 연속한 숫자/maximum-consecutive-number.java
+++ 260405/최대로 연속한 숫자/maximum-consecutive-number.java
@@ -2,25 +2,48 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static class Node implements Comparable<Node>{
+		int len, s, e;
+		
+		Node(int len, int s, int e){
+			this.len = len;
+			this.s = s;
+			this.e = e;
+		}
+		
+		@Override
+		public int compareTo(Node n) {
+			if(this.len != n.len) return n.len - this.len;
+			else if(this.s != n.s) return this.s - n.s;
+			return this.e - n.e;
+		}
+	}
+	
+	static TreeSet<Node> tSet = new TreeSet<>();
+	static TreeSet<Integer> nSet = new TreeSet<>();
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        HashSet<Integer> set = new HashSet<>();
-        set.add(N + 1);
+        // 초기값
+        nSet.add(-1);
+        nSet.add(N + 1);
+        tSet.add(new Node(N + 1, -1, N + 1));
+        
         st = new StringTokenizer(br.readLine());
         for(int i = 0; i < M; i++) {
-        	int prev = 0;
-        	int cnt = 0;
-        	set.add(Integer.parseInt(st.nextToken()));
-        	
-        	for(Integer next : set) {
-        		int num = prev == 0 ? next - prev : next - prev - 1;
-        		cnt = Math.max(cnt, num);
-        		prev = next;
-        	}
-        	System.out.println(cnt);
+        	int y = Integer.parseInt(st.nextToken());
+        	// y 바로 밑에 값
+        	int x = nSet.lower(y);
+        	// y 바로 위에 값
+        	int z = nSet.higher(y);
+        	nSet.add(y);
+        	// x ~ z 범위가 지워지고 x ~ y, y ~ z 2개로 추가됨
+        	tSet.remove(new Node(z - x - 1, x, z));
+        	tSet.add(new Node(y - x - 1, x, y));
+        	tSet.add(new Node(z - y - 1, y, z));
+        	System.out.println(tSet.first().len);
         }
     }
 }
