a71994a65072fec2426bab53e83a3ffe5138f813
kms961101 <63328574+kms961101@users.noreply.github.com>
2026-04-15 20:51:13 +0900

    Title: 범위 내에 있는 점의 수 2, Time: 0ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 260415/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    final static int MAX_N = 1000001;
+	static int N, Q;
+	static int[] arr = new int[MAX_N];
+	static int[] prefix = new int[MAX_N];
+    public static void main(String[] args) throws IOException{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	N = Integer.parseInt(st.nextToken());
+    	Q = Integer.parseInt(st.nextToken());
+    	
+    	st = new StringTokenizer(br.readLine());
+    	for(int i = 0; i < N; i++) {
+    		int idx = Integer.parseInt(st.nextToken());
+    		arr[idx] = 1;
+    	}
+    	
+    	for(int i = 1; i < MAX_N; i++) prefix[i] = prefix[i - 1] + arr[i];
+    	
+    	for(int i = 0; i < Q; i++) {
+    		st = new StringTokenizer(br.readLine());
+    		int A = Integer.parseInt(st.nextToken());
+    		int B = Integer.parseInt(st.nextToken());
+    		System.out.println(prefix[B] - prefix[A - 1]);
+    	}
+    }
+}
