1b79863c2c9afc65f689af57228aaa0b8ff34a5f
kms961101 <63328574+kms961101@users.noreply.github.com>
2026-04-17 18:57:47 +0900

    Title: 마라톤 중간에 택시타기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260417/마라톤 중간에 택시타기/taking-a-taxi-in-the-middle-of-the-marathon.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static class Node{
+		int x, y;
+		
+		Node(int x, int y){
+			this.x = x;
+			this.y = y;
+		}
+	}
+    public static void main(String[] args) throws IOException{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	int N = Integer.parseInt(br.readLine());
+    	StringTokenizer st = null;
+    	
+    	Node[] node = new Node[N + 1];
+    	int[] distL = new int[N + 1];
+    	int[] distR = new int[N + 1];
+    	for(int i = 1; i <= N; i++) {
+    		st = new StringTokenizer(br.readLine());
+    		int x = Integer.parseInt(st.nextToken());
+    		int y = Integer.parseInt(st.nextToken());
+    		node[i] = new Node(x, y);
+    	}
+    	
+    	for(int i = 2; i < N; i++) {
+    		distL[i] = distL[i - 1] + Math.abs(node[i].x - node[i - 1].x) + Math.abs(node[i].y - node[i - 1].y);
+    	}
+    	
+    	for(int i = N - 1; i > 0; i--) {
+    		distR[i] = distL[i + 1] + Math.abs(node[i + 1].x - node[i].x) + Math.abs(node[i + 1].y - node[i].y);
+    	}
+    	int min = Integer.MAX_VALUE;
+    	for(int i = 2; i < N; i++) {
+    		Node prev = node[i - 1];
+    		Node next = node[i + 1];
+    		
+    		int diff = Math.abs(prev.x - next.x) + Math.abs(prev.y - next.y);
+    		min = Math.min(min, distL[i - 1] + distR[i + 1] + diff);
+    	}
+    	System.out.println(min);
+    }
+}
