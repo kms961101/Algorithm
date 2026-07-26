f50e970626b3804be8d08eacbcc92e6c5793d5b8
kms961101 <63328574+kms961101@users.noreply.github.com>
2026-07-26 18:56:06 +0900

    [Passed] 최단 거리 9 (426ms, 30MB)

---
+++ 260726/최단 거리 9/shortest-distance-9.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static final int MAX_N = 1000;
+    static int n, m, A, B;
+    static int[][] dist = new int[MAX_N + 1][MAX_N + 1];;
+    static int[] dp = new int[MAX_N + 1];
+    static int[] path = new int[MAX_N + 1];
+    static boolean[] visited = new boolean[MAX_N + 1];
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        
+        for(int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken());
+            
+            dist[a][b] = dist[b][a] = d;
+        }
+        
+        st = new StringTokenizer(br.readLine());
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+        
+        Arrays.fill(dp, (int)1e9);
+        dp[A] = 0;
+        
+        for(int i = 1; i <= n; i++) {
+            // 아직 방문하지 않은 정점 중
+            // 거리가 최솟값인 정점 찾기
+            int minDist = -1;
+            
+            for(int j = 1; j <= n; j++) {
+                if(visited[j]) continue;
+                
+                if(minDist == -1 || dp[minDist] > dp[j]) {
+                    minDist = j;
+                }
+            }
+            
+            visited[minDist] = true;
+            
+            for(int j = 1; j <= n; j++) {
+                // 간선이 없으면 패스
+                if(dist[minDist][j] == 0) continue;
+                
+                if(dp[j] > dp[minDist] + dist[minDist][j]) {
+                    dp[j] = dp[minDist] + dist[minDist][j];
+                    path[j] = minDist;
+                }
+            }
+        }
+        
+        System.out.println(dp[B]);
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(B);
+        while(A != B) {
+            B = path[B];
+            list.add(B);
+        }
+        for(int i = list.size() - 1; i >= 0; i--) {
+            System.out.print(list.get(i) + " ");
+        }
+    }
+}
